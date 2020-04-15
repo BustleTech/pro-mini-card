5f995941db194603c9ba6fd41671135c95040545
MadhanKumar619 <madhan@codingmart.com>
2020-04-15 22:22:58 +0530

    Updates

---
+++ precache-manifest.357c8ba525411f802ed1098f6b3efbf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9c73a7e90664ba06ff15d80e9635c5",
+    "url": "/pro-mini-card/index.html"
+  },
+  {
+    "revision": "2d08a7939912da8f111a",
+    "url": "/pro-mini-card/static/css/main.c91a408e.chunk.css"
+  },
+  {
+    "revision": "f346d25ae196c7b27cef",
+    "url": "/pro-mini-card/static/js/2.364223da.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pro-mini-card/static/js/2.364223da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d08a7939912da8f111a",
+    "url": "/pro-mini-card/static/js/main.06f3ca8e.chunk.js"
+  },
+  {
+    "revision": "4a073f92af96c8259ef2",
+    "url": "/pro-mini-card/static/js/runtime-main.c1f55552.js"
+  },
+  {
+    "revision": "a5c4ca7d98cf4aa65e2860b4ef809c67",
+    "url": "/pro-mini-card/static/media/Madhan-pro.a5c4ca7d.jpg"
+  }
+]);

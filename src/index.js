b4dd0adfa1d5fe3ffbdfd2e5c38753503ebc4b0e
MadhanKumar619 <madhan@codingmart.com>
2020-04-15 22:30:16 +0530

    Css Fixes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 import "./Assets/css/Home.css";
 import "./Assets/css/style.css"
